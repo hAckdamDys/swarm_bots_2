ef34ce8929b22855b760717339a089ac2a17de70
Adam Dyszy <electro.ubro@gmail.com>
2019-05-28 21:34:14 +0200

    add readme and setup.py

---
+++ readme.md
@@ -0,0 +1,3 @@
+# Multi-robot system coordination using swarm intelligence.
+ 
+## This project's goal is to make swarm of robots realizing simple building tasks without central unit.
---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+with open('readme.md') as f:
+    readme = f.read()
+
+setup(name='SwarmBots',
+      version='0.1',
+      description='Multi-robot system coordination using swarm intelligence.',
+      long_description=readme,
+      url='https://github.com/hAckdamDys/swarm_bots_2',
+      author='Adam Dyszy',
+      author_email='electro.ubro@gmail.com',
+      license='MIT',
+      packages=['SwarmBots']
+      )
