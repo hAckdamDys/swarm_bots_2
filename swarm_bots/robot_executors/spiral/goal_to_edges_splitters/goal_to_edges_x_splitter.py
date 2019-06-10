675ced39be18403bd4065204cb0aafb4b116a511
Adam Dyszy <electro.ubro@gmail.com>
2019-06-11 00:34:59 +0200

    middle diag positions for np done

--- swarm_bots/robot_executors/spiral/goal_to_edges_splitters/goal_to_edges_x_splitter.py
+++ swarm_bots/robot_executors/spiral/goal_to_edges_splitters/goal_to_edges_x_splitter.py
@@ -24,36 +24,25 @@ class GoalToEdgesXSplitter(GoalToEdgesSplitter):
         width = self.goal_building.width
         height = self.goal_building.height
         # first add elements that are not on diagonals
-        test_array = np.zeros((width, height), dtype=int)
-        counts = np.zeros(4, dtype=int)
-        for x in range(width):
-            for y in range(height):
-                # if self
-                if (y <= abs(height - (x * height / width))) and (y <= abs((x * height / width))):
-                    test_array[(x, y)] += 1
-                    counts[0] += 1
-                    # left triangle
-                if (y >= abs((x * height / width))) and (y >= abs(height - (x * height / width))):
-                    test_array[(x, y)] += 10
-                    counts[1] += 1
-                    # right triangle
-                if (y <= abs(height - (x * height / width))) and (y >= abs((x * height / width))):
-                    test_array[(x, y)] += 100
-                    counts[2] += 1
-                    # up triangle
-                if (y >= abs(height - (x * height / width))) and (y <= abs((x * height / width))):
-                    test_array[(x, y)] += 1000
-                    counts[3] += 1
-                    # down triangle
-                # if y >= abs(height-np.floor(x*height/width))-1:
-                #     test_array[(x, y)] += 10
-                # if -y <= height-abs(np.ceil(x*height/width))-1:
-                #     test_array[(x, y)] += 100
-                # if -y >= height-abs(np.floor(x*height/width))-1:
-                #     test_array[(x, y)] += 1000
-        # TODO: delete later
-        test_array = np.flip(test_array, 0)
-        print(test_array, counts)
+        # test_array = np.zeros((width, height), dtype=int)
+        counts = np.zeros(4, dtype=int)  # how many blocks each edge have assigned
+        is_raising_diag = np.zeros((width, height), dtype=bool) # True if diag on field
+        is_decreasing_diag = np.zeros((width, height), dtype=bool)
+        if width == 1 or height == 1:
+            raise ValueError("goal_building should be at least 3x3")
+        if (width % 2 == 1) and (height % 2 == 1):
+            mid_block_x = width//2
+            mid_block_y = height//2
+            mid_block_pos = (mid_block_x, mid_block_y)
+            is_decreasing_diag[mid_block_pos] = True
+            is_raising_diag[mid_block_pos] = True
+            # D  0 R
+            # 0 RD 0
+            # R  0 D
+            is_decreasing_diag[mid_block_x - 1, mid_block_y + 1] = True
+            is_decreasing_diag[mid_block_x + 1, mid_block_y - 1] = True
+            is_raising_diag[mid_block_x + 1, mid_block_y + 1] = True
+            is_raising_diag[mid_block_x - 1, mid_block_y - 1] = True
 
 
 class GoalBuildingMock(GoalBuilding):
