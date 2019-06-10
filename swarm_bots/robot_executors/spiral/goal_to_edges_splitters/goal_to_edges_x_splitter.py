f0b0dd9ece7e4715bf043906950be0cfc928eff6
Adam Dyszy <electro.ubro@gmail.com>
2019-06-11 00:54:00 +0200

    init middle for diags done

--- swarm_bots/robot_executors/spiral/goal_to_edges_splitters/goal_to_edges_x_splitter.py
+++ swarm_bots/robot_executors/spiral/goal_to_edges_splitters/goal_to_edges_x_splitter.py
@@ -26,28 +26,54 @@ class GoalToEdgesXSplitter(GoalToEdgesSplitter):
         # first add elements that are not on diagonals
         # test_array = np.zeros((width, height), dtype=int)
         counts = np.zeros(4, dtype=int)  # how many blocks each edge have assigned
-        is_raising_diag = np.zeros((width, height), dtype=bool) # True if diag on field
+        is_raising_diag = np.zeros((width, height), dtype=bool)  # True if diag on field
         is_decreasing_diag = np.zeros((width, height), dtype=bool)
         if width == 1 or height == 1:
             raise ValueError("goal_building should be at least 3x3")
         if (width % 2 == 1) and (height % 2 == 1):
-            mid_block_x = width//2
-            mid_block_y = height//2
+            mid_block_x = width // 2
+            mid_block_y = height // 2
             mid_block_pos = (mid_block_x, mid_block_y)
             is_decreasing_diag[mid_block_pos] = True
             is_raising_diag[mid_block_pos] = True
             # D  0 R
             # 0 RD 0
             # R  0 D
-            is_decreasing_diag[mid_block_x - 1, mid_block_y + 1] = True
-            is_decreasing_diag[mid_block_x + 1, mid_block_y - 1] = True
-            is_raising_diag[mid_block_x + 1, mid_block_y + 1] = True
-            is_raising_diag[mid_block_x - 1, mid_block_y - 1] = True
+            decreasing_diag_left_pos = (mid_block_x - 1, mid_block_y + 1)
+            is_decreasing_diag[decreasing_diag_left_pos] = True
+            decreasing_diag_right_pos = (mid_block_x + 1, mid_block_y - 1)
+            is_decreasing_diag[decreasing_diag_right_pos] = True
+
+            raising_diag_left_pos = (mid_block_x - 1, mid_block_y - 1)
+            is_raising_diag[raising_diag_left_pos] = True
+            raising_diag_right_pos = (mid_block_x + 1, mid_block_y + 1)
+            is_raising_diag[raising_diag_right_pos] = True
+        else:
+            # D R
+            # R D
+            left_x = width // 2 - 1
+            if height % 2 == 0:
+                down_y = height // 2 - 1
+            else:
+                down_y = height // 2
+            decreasing_diag_left_pos = (left_x, down_y + 1)
+            is_decreasing_diag[decreasing_diag_left_pos] = True
+            decreasing_diag_right_pos = (left_x + 1, down_y)
+            is_decreasing_diag[decreasing_diag_right_pos] = True
+
+            raising_diag_left_pos = (left_x, down_y)
+            is_raising_diag[raising_diag_left_pos] = True
+            raising_diag_right_pos = (left_x + 1, down_y + 1)
+            is_raising_diag[raising_diag_right_pos] = True
+
+        diags = np.flip(is_raising_diag+2*is_decreasing_diag, 0)
+        print("a")
+
 
 
 class GoalBuildingMock(GoalBuilding):
     def __init__(self):
-        self.width = 13
+        self.width = 12
         self.height = 19
 
     def validate_grid(self, base_grid: BaseGrid):
