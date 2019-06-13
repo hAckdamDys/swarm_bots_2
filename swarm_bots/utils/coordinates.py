575d45644abac4a646dffa3cf4f31b72384049b2
Adam Dyszy <electro.ubro@gmail.com>
2019-06-14 01:22:29 +0200

    add possibility to move coordinates that is shared object between classes

--- swarm_bots/utils/coordinates.py
+++ swarm_bots/utils/coordinates.py
@@ -10,17 +10,25 @@ class Coordinates:
         return self.x, self.y
 
     def create_moved_coordinate(self, direction: Direction, move_steps: int):
+        new_coordinate = Coordinates(self.x, self.y)
+        new_coordinate.move_coordinate(direction, move_steps)
+        return new_coordinate
+
+    def create_neighbour_coordinate(self, direction: Direction):
+        return self.create_moved_coordinate(direction, 1)
+
+    def move_coordinate(self, direction: Direction, move_steps: int):
         if direction.is_x_or_y_rising():
             value_to_add = move_steps
         else:
             value_to_add = -move_steps
         if direction.is_x_axis():
-            return Coordinates(self.x + value_to_add, self.y)
+            self.x += value_to_add
         else:
-            return Coordinates(self.x, self.y + value_to_add)
+            self.y += value_to_add
 
-    def create_neighbour_coordinate(self, direction: Direction):
-        return self.create_moved_coordinate(direction, 1)
+    def move_to_neighbour(self, direction: Direction):
+        self.move_coordinate(direction, 1)
 
     def __str__(self):
         return "{x: "+str(self.x)+", y: "+str(self.y)+"}"
